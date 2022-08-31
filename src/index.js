db3e9e6bcef8dfcbe15ecc9526781ddd70263688
ggss-dev <gustavogss.jp@gmail.com>
2022-08-31 20:36:11 -0300

    Deletar conta

--- src/index.js
+++ src/index.js
@@ -100,6 +100,19 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
  const {customer} = req;
  return res.json(customer);
  });
+
+app.delete("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const {customer} = req;
+  customers.splice(customer, 1);
+  return res.status(200).json(customers);
+});
+
+app.get("/balance/create", (req, res) => {
+  const {customer} = req;
+  const balance = getBalance(customer.statement);
+  return response.json(balance);
+});
+
  app.listen(3333);
 
 
